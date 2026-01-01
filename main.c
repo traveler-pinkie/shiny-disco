0c0df2a49f1c91c25ee1edfa993f3ab7e2aa50b0
James Barrera <75712292+traveler-pinkie@users.noreply.github.com>
2026-01-01 03:40:42 +0000

    task 4 & 5 complete

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <linux/if_packet.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <stdio.h>
 
 
 int main(){
 
     int sockfd;
     unsigned char buffer[65536];
-    iphder_t struct ip_header;
-    iphder_t *ip_header;
+    struct iphdr *ip_header;
     if(((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)){
         return -1;
     }
@@ -17,7 +19,15 @@ int main(){
         if(recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL) < 0){
             return -1;
         }
-        ip_header = (iphder_t *)(buffer + 14);
-
+        ip_header = (struct iphdr *)(buffer + 14);
+        
+        struct in_addr source;
+        struct in_addr dest;
+        
+        source.s_addr = ip_header->saddr;
+        dest.s_addr = ip_header->daddr;
+        
+        printf("Source IP: %s\n", inet_ntoa(source));
+        printf("Destination IP: %s\n", inet_ntoa(dest));
     }
 }
